7cf3c61284bb319e891b01b4c59c551269c0782a
jake-eisaguirre <eisaguirre@bren.ucsb.edu>
2023-01-27 11:47:45 -0800

    data download script updated

--- misc/data_download.R
+++ misc/data_download.R
@@ -28,37 +28,37 @@ error=function(cond) {
 dbExecute(connection, "set search_path = survey_data")
 
 location <- dbGetQuery(connection, "select * from location;")
-write_csv(location, here("data", "location.csv"))
+write_csv(location, here("RIBBiTR_DataRepository", "data", "location.csv"))
 
 region <- dbGetQuery(connection, "select * from region;")
-write_csv(region, here("data", "region.csv"))
+write_csv(region, here("RIBBiTR_DataRepository", "data", "region.csv"))
 
 site <- dbGetQuery(connection, "select * from site") 
 site <- select(site, !c(site_id, region_id))
-write_csv(site, here("data", "site.csv"))
+write_csv(site, here("RIBBiTR_DataRepository", "data", "site.csv"))
 
 visit <- dbGetQuery(connection, "select * from visit") %>% 
   drop_na(date) %>% 
   mutate(year = year(date),
          year = )
 visit <- select(visit, !c(site_id, visit_id))
-write_csv(visit, here("data", "visit.csv"))
+write_csv(visit, here("RIBBiTR_DataRepository", "data", "visit.csv"))
 
 survey <- dbGetQuery(connection, "select * from survey")
 survey <- select(survey, !c(visit_id, survey_id))
-write_csv(survey, here("data", "survey.csv"))
+write_csv(survey, here("RIBBiTR_DataRepository", "data", "survey.csv"))
 
 capture <- dbGetQuery(connection, "select * from capture") 
 capture <- select(capture, !c(survey_id, capture_id))
-write_csv(capture, here("data", "capture.csv"))
+write_csv(capture, here("RIBBiTR_DataRepository", "data", "capture.csv"))
 
 ves_cols <- dbGetQuery(connection, "select * from ves")
 ves_cols <- select(ves_cols, !c(survey_id, ves_id))
-write_csv(ves_cols, here("data", "ves_cols.csv"))
+write_csv(ves_cols, here("RIBBiTR_DataRepository", "data", "ves_cols.csv"))
 
 aural_cols <- dbGetQuery(connection, "select * from aural")
 aural_cols <- select(aural_cols, !c(survey_id, aural_id))
-write_csv(aural_cols, here("data", "aural_cols.csv"))
+write_csv(aural_cols, here("RIBBiTR_DataRepository", "data", "aural_cols.csv"))
 
 ###### CAP ######
 raw_cap  <- dbGetQuery(connection, "select l.*, r.*, s.*, v.*, su.*, c.*
@@ -74,7 +74,7 @@ cap <- raw_cap %>%
   select(!c(location_id, region_id, site_id, visit_id, survey_id)) %>% 
   drop_na(date) %>% 
   mutate(year = year(date))
-write_csv(cap, here("data", "cap.csv"))
+write_csv(cap, here("RIBBiTR_DataRepository", "data", "cap.csv"))
 
 ################
 
@@ -88,19 +88,19 @@ sn_bd <- dbGetQuery(connection, "select * from sierra_nevada_bd")
 pan_bd <- dbGetQuery(connection, "select * from panama_bd_temp")
 
 comb_bd <- plyr::rbind.fill(serdp_bd, sn_bd, pan_bd) 
-write_csv(comb_bd, here("data", "comb_bd.csv"))
+write_csv(comb_bd, here("RIBBiTR_DataRepository", "data", "comb_bd.csv"))
 
 # AMP #
 serdp_amp <- dbGetQuery(connection, "select * from serdp_amp")
-write_csv(serdp_amp, here("data", "serdp_amp.csv"))
+write_csv(serdp_amp, here("RIBBiTR_DataRepository", "data", "serdp_amp.csv"))
 
 # Muc/microbiome #
 serdp_muc_mic <- dbGetQuery(connection, "select * from serdp_newt_microbiome_mucosome_antifungal") 
-write_csv(serdp_muc_mic, here("data", "serdp_muc_mic.csv"))
+write_csv(serdp_muc_mic, here("RIBBiTR_DataRepository", "data", "serdp_muc_mic.csv"))
 
 # Bd GENETIC #
 serdp_bd_genom <- dbGetQuery(connection, "select * from serdp_bd_genomic")
-write_csv(serdp_bd_genom, here("data", "serdp_bd_genom.csv"))
+write_csv(serdp_bd_genom, here("RIBBiTR_DataRepository", "data", "serdp_bd_genom.csv"))
 
 ##### VES #######
 raw_ves <- dbGetQuery(connection, "select l.*, r.*, s.*, v.*, su.*, c.*
@@ -114,7 +114,7 @@ ves <- raw_ves %>%
   select(!c(location_id, region_id, site_id, visit_id, survey_id)) %>% 
   drop_na(date) %>% 
   mutate(year = year(date))
-write_csv(ves, here("data", "ves.csv"))
+write_csv(ves, here("RIBBiTR_DataRepository", "data", "ves.csv"))
 ###############
 
 ####### AURAL ########
@@ -129,7 +129,7 @@ aural <- raw_aural %>%
   select(!c(location_id, region_id, site_id, visit_id, survey_id)) %>% 
   drop_na(date) %>% 
   mutate(year = year(date))
-write_csv(aural, here("data", "aural.csv"))
+write_csv(aural, here("RIBBiTR_DataRepository", "data", "aural.csv"))
 #################
 
 ######### hobo ########
@@ -144,21 +144,55 @@ raw_hobo <- dbGetQuery(connection, "select hl.*, hr.*, hs.*, h.*
 hobo <- raw_hobo %>% 
   select(!c(hobo_location_id, hobo_region_id, hobo_location_id..5, hobo_site_id, hobo_region_id..13, hobo_id, hobo_site_id..25)) %>% 
   mutate(year = year(date_time))
-write_csv(hobo, here("data", "hobo.csv"))
+write_csv(hobo, here("RIBBiTR_DataRepository", "data", "hobo.csv"))
 
 hobo_location <- dbGetQuery(connection, "select location from hobo_location")
-write_csv(hobo_location, here("data", "hobo_location.csv"))
+write_csv(hobo_location, here("RIBBiTR_DataRepository", "data", "hobo_location.csv"))
 
 hobo_region <- dbGetQuery(connection, "select region from hobo_region")
-write_csv(hobo_region, here("data", "hobo_region.csv"))
+write_csv(hobo_region, here("RIBBiTR_DataRepository", "data", "hobo_region.csv"))
 
 hobo_site <- dbGetQuery(connection, "select * from hobo_site")  
 hobo_site <- select(hobo_site, !c(hobo_site_id, hobo_region_id))
-write_csv(hobo_site, here("data", "hobo_site.csv"))
+write_csv(hobo_site, here("RIBBiTR_DataRepository", "data", "hobo_site.csv"))
 
 hobo_cols <- dbGetQuery(connection, "select * from hobo")
 hobo_cols <- select(hobo_cols, !c(hobo_id, hobo_site_id)) %>% 
   mutate(year = year(date_time))
-write_csv(hobo_cols, here("data", "hobo_cols.csv"))
+write_csv(hobo_cols, here("RIBBiTR_DataRepository", "data", "hobo_cols.csv"))
 
 #################
+
+######## Audio data ##############
+dbExecute(connection, "set search_path = audio")
+
+raw_audio <- dbGetQuery(connection,
+                        "select al.location, ar.region, as2.site,
+                         av.date_of_deployment, ai.*
+                         from audio_location al 
+                         join audio_region ar on al.location_id = ar.location_id 
+                         join audio_site as2 on ar.region_id = as2.region_id 
+                         join audio_visit av on as2.site_id = av.site_id 
+                         join audio_info ai on av.visit_id = ai.visit_id;")
+
+audio <- raw_audio %>% 
+  select(!c(audio_id, visit_id))
+write_csv(audio, here("RIBBiTR_DataRepository", "data", "audio.csv"))
+
+audio_location <- dbGetQuery(connection, "select location from audio_location;")
+write_csv(audio_location, here("RIBBiTR_DataRepository", "data", "audio_location.csv"))
+
+audio_region <- dbGetQuery(connection, "select region from audio_region;")
+write_csv(audio_region, here("RIBBiTR_DataRepository", "data", "audio_region.csv"))
+
+audio_site <- dbGetQuery(connection, "select site from audio_site;")
+write_csv(audio_site, here("RIBBiTR_DataRepository", "data", "audio_site.csv"))
+
+audio_visit <- dbGetQuery(connection, "select date_of_deployment from audio_visit;")
+write_csv(audio_visit, here("RIBBiTR_DataRepository", "data", "audio_visit.csv"))
+
+audio_cols <- dbGetQuery(connection, "select * from audio_info")
+audio_cols <- select(audio_cols, !c(audio_id, visit_id))
+write_csv(audio_cols, here("RIBBiTR_DataRepository", "data", "audio_cols.csv"))
+
+############# END Audio ############################
